d5769d8352394ac926783594a95f50418da59449
Manasi <manudee@gmail.com>
2017-09-22 22:31:54 -0700

    commented out console logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,8 +26,8 @@ mainScore =  randomGenerator(19,120);
 $(".mainScore").html();
 
 $("#mainScore").html( "<h2>" + mainScore + "</h2>" );
-console.log("Main Score " + mainScore);
-console.log("TotalScore " + totalScore);
+//console.log("Main Score " + mainScore);
+//console.log("TotalScore " + totalScore);
 $("#totalScore").html( "<h2>" + totalScore + "</h2>" );
 });
 
@@ -39,13 +39,13 @@ crystal3Value = randomGenerator(1,12);
 crystal4Value = randomGenerator(1,12);
 mainScore =  randomGenerator(19,120);
 totalScore = 0;
-console.log("in Crystal Random " );
-console.log("Main Score " + mainScore);
-console.log("totalSCore " + totalScore);
-console.log("crystal1Value " + crystal1Value);
-console.log("crystal2Value " + crystal2Value);
-console.log("crystal3Value " + crystal3Value);
-console.log("crystal4Value " + crystal4Value);
+//console.log("in Crystal Random " );
+//console.log("Main Score " + mainScore);
+//console.log("totalSCore " + totalScore);
+//console.log("crystal1Value " + crystal1Value);
+//console.log("crystal2Value " + crystal2Value);
+//console.log("crystal3Value " + crystal3Value);
+//console.log("crystal4Value " + crystal4Value);
 $("#mainScore").html( "<h2>" + mainScore + "</h2>" );
 $("#totalScore").html( "<h2>" + totalScore + "</h2>" );
 }
@@ -54,20 +54,20 @@ $("#totalScore").html( "<h2>" + totalScore + "</h2>" );
 
 function checker(){
 	if (totalScore > mainScore){
-		console.log("You lose")
+		//console.log("You lose")
 		losses++;
         $("#losses").html("<h2>" + losses + "</h2>" );
-		console.log("Losses " + losses);
+		//console.log("Losses " + losses);
 		crystalRandom();
         
 
 		}
 		//losses++, call Reset function
 	else if (totalScore === mainScore ){
-		console.log("You win")	
+		//console.log("You win")	
 		wins++;
         $("#wins").html("<h2>" + wins + "</h2>");
-		console.log("Wins " + wins);
+		//console.log("Wins " + wins);
 		crystalRandom();
         
 		}
@@ -77,11 +77,11 @@ function checker(){
  $(document).ready(function() {
     $("#crystal1" ).on("click", function() {
           
-            console.log("clicked on crystal1")
+            //console.log("clicked on crystal1")
             
-            console.log("crystal1Value " + crystal1Value);
+            //console.log("crystal1Value " + crystal1Value);
             totalScore = totalScore + crystal1Value;
-            console.log("totalSCore " + totalScore);
+            //console.log("totalSCore " + totalScore);
             $("#totalScore").html( "<h2>" + totalScore + "</h2>" );
             checker();
     });
@@ -91,10 +91,10 @@ function checker(){
 $(document).ready(function() {
     $("#crystal2" ).on("click", function() {
            
-            console.log("clicked on crystal2")
-            console.log("crystal2Value " + crystal2Value);
+            //console.log("clicked on crystal2")
+            //console.log("crystal2Value " + crystal2Value);
             totalScore = totalScore + crystal2Value;
-            console.log("totalSCore " + totalScore);
+            //console.log("totalSCore " + totalScore);
             $("#totalScore").html( "<h2>" + totalScore + "</h2>" );
             checker();
     });
@@ -104,10 +104,10 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#crystal3" ).on("click", function() {
            
-            console.log("clicked on crystal3")
-            console.log("crystal3Value " + crystal3Value);
+            //console.log("clicked on crystal3")
+            //console.log("crystal3Value " + crystal3Value);
             totalScore = totalScore + crystal3Value;
-            console.log("totalSCore " + totalScore);
+            //console.log("totalSCore " + totalScore);
             $("#totalScore").html( "<h2>" + totalScore + "</h2>" );
             checker();
     });
@@ -117,10 +117,10 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#crystal4" ).on("click", function() {
            
-            console.log("clicked on crystal4")
-            console.log("crystal4Value " + crystal4Value);
+            //console.log("clicked on crystal4")
+            //console.log("crystal4Value " + crystal4Value);
             totalScore = totalScore + crystal4Value;
-            console.log("totalSCore " + totalScore);
+            //console.log("totalSCore " + totalScore);
             $("#totalScore").html( "<h2>" + totalScore + "</h2>" );
             checker();
     });
